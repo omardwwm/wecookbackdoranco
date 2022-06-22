c64ef19023a0b66ae317b02c2ee5faf8345e64f6
omardwwm <boudraa.omar@gmail.com>
2022-06-22 17:34:37 +0200

    add userController

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -31,25 +31,29 @@ const { s3Config, multerS3Config, checkFileType } = require("../s3")
 //   }
 
 // }
+function checkFileTypes(file, cb) {
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(
+    path.extname(file.originalname).toString()
+  );
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
 
 // Init upload 
 const upload = multer({
   storage: multerS3Config,
   fileFilter: function (req, file, cb) {
-    // extension autorisee
-    const fileTypes = /jpeg|jpg|png|jfif|gif|webp/;
-    // verfifier l'exte
-    const extname = fileTypes.test(path.parse(file.originalname).ext);
-    // verfier le mime
-    const mimetype = fileTypes.test(file.mimeType);
-
-    if (mimetype && extname) {
-      return cb(null, true);
-    } else {
-      cb("Error: type du fichier invalide !");
-    }
-  }
-})
-  .single("profilePicture");
+    checkFileTypes(file, cb);
+  },
+}).single("profilePicture");
 
 module.exports = upload; 
